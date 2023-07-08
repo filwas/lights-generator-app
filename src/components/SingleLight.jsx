a1eefa214fcf5a3dfe2b870ef536fa63f45d490a
Filip Was <fwas.corp@gmail.com>
2023-07-08 19:16:39 +0200

    light color is now random

--- src/components/SingleLight.jsx
+++ src/components/SingleLight.jsx
@@ -1,17 +1,31 @@
-import { useState } from "react"
-import LightTurnedOff from "/images/light-turned-off.svg"
-import LightBlue from "/images/light-blue.svg"
+import { useState } from "react";
+import LightTurnedOff from "/images/light-turned-off.svg";
+import LightBlue from "/images/light-blue.svg";
+import LightGreen from "/images/light-green.svg";
+import LightOrange from "/images/light-orange.svg";
+import LightPink from "/images/light-pink.svg";
+import LightRed from "/images/light-red.svg";
+import LightYellow from "/images/light-yellow.svg";
+
+const lights = [
+  LightBlue,
+  LightGreen,
+  LightOrange,
+  LightPink,
+  LightRed,
+  LightYellow,
+];
 
 export const SingleLight = () => {
-    const[isOn, setOn] = useState(true);
+  const [isOn, setOn] = useState(true);
 
-    const imageSrc = isOn ? LightBlue : LightTurnedOff;
+  const imageSrc = isOn
+    ? lights[Math.floor(Math.random() * lights.length)]
+    : LightTurnedOff;
 
-    const handleToggle = () => {
-        setOn(!isOn)
-    }
+  const handleToggle = () => {
+    setOn(!isOn);
+  };
 
-    return(
-        <img onClick={handleToggle} src={imageSrc} alt="Single light" />
-    )
-}
+  return <img onClick={handleToggle} src={imageSrc} alt="Single light" />;
+};
